0c7d1084dbaaed415803ec0789556adb50216258
Matt Jones <gitcode@magisa.org>
2014-09-05 13:57:38 -0800

    Load csv data in test block.

--- inst/tests/test_variance_ratio.R
+++ inst/tests/test_variance_ratio.R
@@ -1,4 +1,4 @@
-context("calVR")
+context("variance ratio")
 
 test_that("calVR function returns correct result", {
     # Ensure that tests 
@@ -6,6 +6,6 @@ test_that("calVR function returns correct result", {
     
     # Load our example data set
     # data("knz_001d", package="codyn")  # This doesn't work for CSV files :(
-    knz_001d <- read.csv("data/knz_001d.csv", sep=",", header=TRUE)
+    knz_001d <- read.csv("../../data/knz_001d.csv", sep=",", header=TRUE)
     expect_that(names(knz_001d)[4], equals("abundance"))
 })
