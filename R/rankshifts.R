b00b1021a93b55c8089b2f1f94915f07f5d13a58
Andrew <a.a.m.macdonald@gmail.com>
2014-09-05 14:46:16 -0700

    documentation for getintersect and add calculation of MRS to the end

--- R/rankshifts.R
+++ R/rankshifts.R
@@ -1,12 +1,9 @@
-#' Function for calculating mean rank shifts
+#' Create intersected data frames
 #'
-#' This is a function that calculates mean rank shifts
-#' @param comm_data Community data
-#' @param timevar the Time variable
-#' @param speciesvar the Species variable
-#' @return the Truth
-
-
+#' Create intersections.
+#' @param d1 A dataframe
+#' @param d2 A dataframe
+#' @param dataname The name of the column on which the two datasets will be joined and intersected
 getintersect <- function(d1, d2, dataname = "species"){
   commspp <- intersect(d1[[dataname]], d2[[dataname]])
   names(d1)[-1] <- paste0(names(d1)[-1],"1")
@@ -15,7 +12,18 @@ getintersect <- function(d1, d2, dataname = "species"){
         y = d2[d2[[dataname]] %in% commspp, ])
 }
 
-meanrank <- function(comm_data = dat, timevar = "year", speciesvar = "species"){
+
+#' Function for calculating mean rank shifts
+#'
+#'
+#' This is a function that calculates mean rank shifts
+#' @param comm_data Community dataset. Must be in 'long' format.
+#' @param timevar The time variable
+#' @param speciesvar The species variable
+#' @param abdvar The abundance variable
+#' @return a dataframe, showing years compared
+meanrank <- function(comm_data = dat, timevar = "year",
+                     speciesvar = "species", abdvar = "abundance"){
   ## split data by year
   yearlist <- split(dat, dat[["year"]])
   ## Compare consecutive pairs of years
@@ -23,12 +31,23 @@ meanrank <- function(comm_data = dat, timevar = "year", speciesvar = "species"){
   y2 <- yearlist[-1]
 
   commonspp <- Map(getintersect, y1, y2)
-  combineddata <- do.call(rbind, commonspp)
 
-  ## generate names to be combined
+  names(commonspp) <- Map(function(x, y) paste0(x, "-", y), names(y1), names(y2))
 
-  transform(combineddata, tipepair = paste0("year1"
+  abdname1 <- paste0(abdvar,"1")
+  abdname2 <- paste0(abdvar,"2")
 
+  ranknames <- lapply(commonspp, function(x) cbind(x,
+                                                   rank1 = rank(x[[abdname1]]),
+                                                   rank2 = rank(x[[abdname2]])
+  ))
+
+  rankdiff <- lapply(ranknames,
+                     function(x) transform(x, abs_ch_rank = abs(rank2 - rank1)))
+
+  MRS <- sapply(rankdiff, function(x) mean(x$abs_ch_rank))
+
+  data.frame(year_pair = names(MRS), MRS, row.names = NULL)
 }
 
 
