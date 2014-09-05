f3b8a381800aefafc5fbf2199ca51361967eefe2
Andrew <a.a.m.macdonald@gmail.com>
2014-09-05 13:09:16 -0700

    deleted references to old datasets

--- R/mean_rank_shift_dplyr.R
+++ R/mean_rank_shift_dplyr.R
@@ -1,11 +1,13 @@
-###dplyr version
-require(dplyr)
-require(tidyr)
+#' Function for obtaining an example dataset
+#'
+#' @
 
-SEV004.sc <- read.csv("SEV004_sc.csv", header = TRUE, strip.white = TRUE)
+getexample <- function(){
+  dat <- read.csv("data/knz_001d.csv", header = TRUE, strip.white = TRUE)
+  subset(dat$subplot == "A_1", drop = TRUE)
+}
 
-veg <- SEV004.sc
-veg
+
 
 
 pairwiseMRS <- data.frame(cbind(year=numeric(0), mrs=numeric(0)))
@@ -13,13 +15,13 @@ yr <- sort(unique(veg$year))
 
 for (i in 2:(length(yr))){
 	veg %>%
-	filter(cover>0) %>% 
+	filter(cover>0) %>%
 	filter(year == yr[i] | year == yr[i-1]) %>%
-	
+
 	mutate(pres = 1)
 	subbercpount <- summarise(pres~species, sum)
-	subboth <- subset(subbercount, subbercount$pres == 2) 
-	
+	subboth <- subset(subbercount, subbercount$pres == 2)
+
 }
 
 clean_veg <- veg %>%
@@ -27,6 +29,6 @@ clean_veg <- veg %>%
   arrange(year, cover) %>%
 	filter(cover>0) %>%
 	summarise(group_by(year), mean(cover))
-	
+
 
 percent_rank(veg$cover)
