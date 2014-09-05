a2c0dd484288b4b2e88439613aa3f07e019568b9
Matt Jones <gitcode@magisa.org>
2014-09-05 11:58:38 -0800

    Fixed syntax error in comment.

--- R/apparent_rate_ext_imm.R
+++ R/apparent_rate_ext_imm.R
@@ -10,8 +10,7 @@
 # If presence / absence is a vector over time, 
 # the function "diff" (lag difference; the diff between a value and the preceding one) does a good job.
 
-x<-c(0,1,1,0,0,0,1,0,1,0,1,0,0,1) # the species went extinct 4 times  
-and re-colonised 5 times.
+x<-c(0,1,1,0,0,0,1,0,1,0,1,0,0,1) # the species went extinct 4 times and re-colonised 5 times.
 
 sum(diff(x)==1) #for immigration; it says how many time the lag difference = 1
 
