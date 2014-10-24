874bae0dd370355288cdfce43d9a368e308d15e9
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-10-24 16:42:03 -0700

    function to create a time series from a longform abundance dataframe

--- R/utilities.R
+++ R/utilities.R
@@ -14,3 +14,25 @@ calComDat<-function(data1, species, year, abundance){
   comdat[year]<-NULL
   return(comdat)
 }
+
+
+#' A function to covert from a longform abundance dataframe to a species x year time series
+#'
+#' @param data1 A dataframe containing year, species and abundance columns
+#' @param year The name of the year column from data1
+#' @param species The name of the species column from data1
+#' @param abundance The name of the abundance column from data1
+#' @return comdat A dataframe of species abundances x year
+calComMat<-function(data1, species, year, abundance){
+  data1 <- data1[order(data1[year]), ]
+  fstr <- paste(species, "~", year, sep="")
+  f <- as.formula(fstr)
+  comdat <- as.data.frame(cast(data1, f, value = abundance, fill=0))
+  rownames(comdat) <- comdat[[species]]
+  comdat[species] <- NULL
+  comdat <- as.matrix(comdat)
+  start_time <- min(as.numeric(colnames(comdat)))
+  end_time <- max(as.numeric(colnames(comdat)))
+  comdat <- as.ts(comdat, start = start_time, end = end_time)
+  return(comdat)
+}
