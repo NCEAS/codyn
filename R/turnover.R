b0f2a897ea0e3519343302cfe1a7cf4110054918
Lauren Hallett <lauren.hallett@berkeley.edu>
2014-10-24 13:11:00 -0600

    adds turnover function between two timepoints within a rep

---
+++ R/turnover.R
@@ -0,0 +1,28 @@
+library(reshape)
+
+#' A function to calculate species turnover between two years within a rep
+#'
+#' @param data1 A dataframe containing year, rep, species and abundance columns
+#' @param year The name of the year column from data1
+#' @param rep The name of the replicate column from data1
+#' @param species The name of the species column from data1
+#' @param abundance The name of the abundance column from data1
+#' @return turnover The species turnover (appearances + disappearances) relative to the total species richness observed across both years 
+
+pairwise_turnover<-function(data1, rep, species, year, abundance){
+d1<-data1[which(data1["abundance"]>0),]
+fstr<-(paste(species, "+", rep, "~", year, sep=""))
+f<-as.formula(fstr)
+d2<-as.data.frame(cast(d1, f, value=abundance, fill=0))
+d2["disapp"]<-ifelse(d2[3]>0 & d2[4]==0, 1, 0)
+d2["app"]<-ifelse(d2[3]==0 & d2[4]>0, 1, 0)
+disapp<-sum(d2["disapp"])
+app<-sum(d2["app"])
+sppchange<-sum(disapp, app)
+totspp<-as.numeric(nrow(d2))
+reldis<-disapp/totspp
+relapp<-app/totspp
+turnover<-sppchange/totspp
+return(turnover)
+}
+
