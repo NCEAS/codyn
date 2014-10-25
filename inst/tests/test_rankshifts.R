c8f8d66f8d21a705bff46a4859808162c210c272
Matt Jones <gitcode@magisa.org>
2014-10-24 19:25:47 -0800

    Added test framework files for rankshifts, timelag, turnover, and utilities.
    The tests still need to be fleshed out, but the infrastructure is in place.  This starts work on the testing issues, including #21, #22, #23, #24, and #25.

---
+++ inst/tests/test_rankshifts.R
@@ -0,0 +1,13 @@
+context("rankshifts")
+
+test_that("rankshifts loads and returns correct result", {
+    # Ensure that trivial tests work correctly
+    expect_that(length("a"), equals(1))
+    
+    library(codyn)
+    
+    # Load our example data set
+    # data("knz_001d", package="codyn")  # This doesn't work for CSV files :(
+    knz_001d <- read.csv("../../data/knz_001d.csv", sep=",", header=TRUE)
+    expect_that(names(knz_001d)[4], equals("abundance"))
+})
