a1918194cf5818c4b31412c2bf032cca74d42a5c
Lauren Hallett <lauren.hallett@berkeley.edu>
2014-10-27 14:52:16 -0600

    corrects test for new function name and options

--- inst/tests/test_variance_ratio.R
+++ inst/tests/test_variance_ratio.R
@@ -1,16 +1,7 @@
 context("variance ratio")
 
-# Utility function to run calVR2 on multiple segments of a data frame
-calVRs<-function(data1, rep, species, year, abundance){
-    X <- split(data1, data1[rep])
-    out<-lapply(X, FUN=calVR2, species, year, abundance)
-    output<-cbind((names(out)), as.data.frame(unlist(out)))
-    names(output)<-c(rep, "VR")
-    row.names(output)<-NULL
-    return(output)
-}
 
-test_that("calVR function returns correct result", {
+test_that("VR function returns correct result", {
     # Ensure that tests 
     expect_that(length("a"), equals(1))
     
@@ -18,20 +9,20 @@ test_that("calVR function returns correct result", {
     # data("knz_001d", package="codyn")  # This doesn't work for CSV files :(
     knz_001d <- read.csv("../../data/knz_001d.csv", sep=",", header=TRUE)
     expect_that(names(knz_001d)[4], equals("abundance"))
-    myresults<-calVRs(knz_001d, "subplot", "species", "year", "abundance")
+    myresults<-VR(knz_001d, "subplot", "species", "year", "abundance", 1)
     expect_that(class(myresults), equals("data.frame"))
-    expect_that(nrow(myresults), equals(20))
+    expect_that(nrow(myresults), equals(1))
     
     #test that it also works with alternate column names
     knz_001d2<-knz_001d
     names(knz_001d2)=c("sp", "yr", "sub", "abund")
-    myresults2<-calVRs(knz_001d2, "sub", "sp", "yr", "abund")
+    myresults2<-VR(knz_001d2, "sub", "sp", "yr", "abund", 1)
     expect_that(sum(myresults2$VR), equals(sum(myresults$VR)))
     
     #test that it works even if there are additional unused columns
     knz_001d3<-knz_001d
     knz_001d3$site<-"KNZ"
-    myresults3<-calVRs(knz_001d3, "subplot", "species", "year", "abundance")
-    expect_that(myresults3, is_identical_to(myresults))
+    myresults3<-VR(knz_001d3, "subplot", "species", "year", "abundance", 1)
+    expect_that(myresults3$VR, is_identical_to(myresults$VR))
   
     })
