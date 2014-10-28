827df7f5ddbb37850e21e5c226b0cdbff1f2da59
Matt Jones <gitcode@magisa.org>
2014-10-27 21:05:57 -0800

    Reindent for formating consistency.

--- R/synchrony.R
+++ R/synchrony.R
@@ -8,16 +8,14 @@
 #' @return output The degree of species synchrony, where 1 is perfect synchrony and 0 is perfect asynchrony
 #' @import reshape
 #' @export
-
 synchrony<-function(data1, rep, species, year, abundance){
-  X <- split(data1, data1[rep])
-  out<-lapply(X, FUN=synch_onerep, species, year, abundance)
-  reps<-unique(data1[rep])
-  output<-cbind(reps, do.call("rbind", out))
-  names(output)=c(rep, "synchrony")
-  return(output)
+    X <- split(data1, data1[rep])
+    out<-lapply(X, FUN=synch_onerep, species, year, abundance)
+    reps<-unique(data1[rep])
+    output<-cbind(reps, do.call("rbind", out))
+    names(output)=c(rep, "synchrony")
+    return(output)
 }
-
 
 #' A function to calculate species synchrony over time within one replicate
 #'
@@ -28,27 +26,26 @@ synchrony<-function(data1, rep, species, year, abundance){
 #' @return output The degree of species synchrony, where 1 is perfect synchrony and 0 is perfect asynchrony
 #' @import reshape
 #' @export
-
-synch_onerep<-function(data1, species, year, abundance){
-#fill in 0s
-fill0formula<-as.formula(paste(species, "~", year))
-datacast<-cast(species~year, value=abundance, data=data1, fill=0)
-data2<-melt(datacast)
-names(data2)=c(species, abundance, year)
-
-#calculate community variance
-XTformula<-as.formula(paste(abundance, "~", year, sep=""))
-XT<-aggregate(XTformula, data=data2, sum )
-#do this within rep
-varXT<-var(XT[abundance])
-
-#calculate species variance
-sdSppformula<-as.formula(paste(abundance, "~", species, sep=""))
-sdSpp<-aggregate(sdSppformula, data=data2, sd)
-varSpp<-sum((sdSpp[abundance]))*sum(sdSpp[abundance])
-
-#calculate synchrony
-synchrony<-as.numeric(varXT/varSpp)
-return(synchrony)
+synch_onerep<-function(data1, species, year, abundance) {
+    #fill in 0s
+    fill0formula<-as.formula(paste(species, "~", year))
+    datacast<-cast(species~year, value=abundance, data=data1, fill=0)
+    data2<-melt(datacast)
+    names(data2)=c(species, abundance, year)
+    
+    #calculate community variance
+    XTformula<-as.formula(paste(abundance, "~", year, sep=""))
+    XT<-aggregate(XTformula, data=data2, sum )
+    #do this within rep
+    varXT<-var(XT[abundance])
+    
+    #calculate species variance
+    sdSppformula<-as.formula(paste(abundance, "~", species, sep=""))
+    sdSpp<-aggregate(sdSppformula, data=data2, sd)
+    varSpp<-sum((sdSpp[abundance]))*sum(sdSpp[abundance])
+    
+    #calculate synchrony
+    synchrony<-as.numeric(varXT/varSpp)
+    return(synchrony)
 }
 
