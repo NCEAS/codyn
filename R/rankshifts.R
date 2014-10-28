0c85c0b2b1606625b2c5be85f91909a9772479b1
Matt Jones <gitcode@magisa.org>
2014-10-27 22:25:34 -0800

    Fix indenting in file.

--- R/rankshifts.R
+++ R/rankshifts.R
@@ -5,11 +5,11 @@
 #' @param d2 A dataframe
 #' @param dataname The name of the column on which the two datasets will be joined and intersected
 getintersect <- function(d1, d2, dataname = "species"){
-  commspp <- intersect(d1[[dataname]], d2[[dataname]])
-  names(d1)[-1] <- paste0(names(d1)[-1],"1")
-  names(d2)[-1] <- paste0(names(d2)[-1],"2")
-  merge(x = d1[d1[[dataname]] %in% commspp, ],
-        y = d2[d2[[dataname]] %in% commspp, ])
+    commspp <- intersect(d1[[dataname]], d2[[dataname]])
+    names(d1)[-1] <- paste0(names(d1)[-1],"1")
+    names(d2)[-1] <- paste0(names(d2)[-1],"2")
+    merge(x = d1[d1[[dataname]] %in% commspp, ],
+          y = d2[d2[[dataname]] %in% commspp, ])
 }
 
 
@@ -25,30 +25,30 @@ getintersect <- function(d1, d2, dataname = "species"){
 #' @export
 meanrank <- function(comm_data, year = "year",
                      species = "species", abundance = "abundance"){
-  ## split data by year
-  yearlist <- split(comm_data, comm_data[[year]])
-  ## Compare consecutive pairs of years
-  y1 <- yearlist[-length(yearlist)]
-  y2 <- yearlist[-1]
-
-  commonspp <- Map(getintersect, y1, y2)
-
-  names(commonspp) <- Map(function(x, y) paste0(x, "-", y), names(y1), names(y2))
-
-  abdname1 <- paste0(abundance,"1")
-  abdname2 <- paste0(abundance,"2")
-
-  ranknames <- lapply(commonspp, function(x) cbind(x,
-                                                   rank1 = rank(x[[abdname1]]),
-                                                   rank2 = rank(x[[abdname2]])
-  ))
-
-  rankdiff <- lapply(ranknames,
-                     function(x) transform(x, abs_ch_rank = abs(rank2 - rank1)))
-
-  MRS <- sapply(rankdiff, function(x) mean(x$abs_ch_rank))
-
-  data.frame(year_pair = names(MRS), MRS, row.names = NULL)
+    ## split data by year
+    yearlist <- split(comm_data, comm_data[[year]])
+    ## Compare consecutive pairs of years
+    y1 <- yearlist[-length(yearlist)]
+    y2 <- yearlist[-1]
+    
+    commonspp <- Map(getintersect, y1, y2)
+    
+    names(commonspp) <- Map(function(x, y) paste0(x, "-", y), names(y1), names(y2))
+    
+    abdname1 <- paste0(abundance,"1")
+    abdname2 <- paste0(abundance,"2")
+    
+    ranknames <- lapply(commonspp, function(x) cbind(x,
+                                                     rank1 = rank(x[[abdname1]]),
+                                                     rank2 = rank(x[[abdname2]])
+    ))
+    
+    rankdiff <- lapply(ranknames,
+                       function(x) transform(x, abs_ch_rank = abs(rank2 - rank1)))
+    
+    MRS <- sapply(rankdiff, function(x) mean(x$abs_ch_rank))
+    
+    data.frame(year_pair = names(MRS), MRS, row.names = NULL)
 }
 
 
