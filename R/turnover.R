4e3ecc2ee72c9ba63b23c20c6d78f5583a489e9a
Lauren Hallett <lauren.hallett@berkeley.edu>
2014-10-29 16:05:46 -0600

    adds function to calculate turnover across replicates

--- R/turnover.R
+++ R/turnover.R
@@ -1,3 +1,26 @@
+#' A function to calculate species turnover between years
+#'
+#' @param data1 A dataframe containing year,species and abundance columns
+#' @param rep The name of the replicate column from data1
+#' @param species The name of the species column from data1
+#' @param year The name of the year column from data1
+#' @param abundance The name of the abundance column from data1
+#' @param metric The turnover metric to return; the default, turnover, returns summed appearances and disappearances relative to total species richness across both years
+#'          appearance returns the number of appearances in the second year relative to total species richness across both years
+#'          disappearance returns the number of disappearances in the second year relative to the total species richness across both years
+#' @return output A dataframe containing the specificed turnover metric and year
+#' @export
+turnover<-function(data1, rep, species, year, abundance, metric="turnover") {
+  X <- split(data1, data1[rep])
+  out<-lapply(X, FUN=turnover_allyears, species, year, abundance, metric)
+  ID<-unique(names(out))
+  out<-mapply(function(x, y) "[<-"(x, rep, value = y) ,
+              out, ID, SIMPLIFY = FALSE)
+  output<-do.call("rbind", out)
+  return(output)
+}
+
+
 #' A function to calculate species turnover between two years 
 #'
 #' @param d1 A dataframe containing a species column from one year
