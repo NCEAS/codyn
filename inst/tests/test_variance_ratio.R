dee9d5fc78a4af1a6bf7513406e82b8bde37df06
Lauren Hallett <lauren.hallett@berkeley.edu>
2014-11-06 11:09:54 -0700

    updates variance ratio test code to reflect new function name (varianceratio not VR)

--- inst/tests/test_variance_ratio.R
+++ inst/tests/test_variance_ratio.R
@@ -1,7 +1,7 @@
 context("variance ratio")
 
 
-test_that("VR function returns correct result", {
+test_that("varianceratio function returns correct result", {
     # Ensure that tests 
     expect_that(length("a"), equals(1))
     
@@ -9,20 +9,20 @@ test_that("VR function returns correct result", {
     # data("knz_001d", package="codyn")  # This doesn't work for CSV files :(
     knz_001d <- read.csv(system.file("extdata", "knz_001d.csv", package="codyn"), sep=",", header=TRUE)
     expect_that(names(knz_001d)[4], equals("abundance"))
-    myresults<-VR(knz_001d, "subplot", "species", "year", "abundance", 1)
+    myresults<-varianceratio(knz_001d, "subplot", "species", "year", "abundance", 1)
     expect_that(class(myresults), equals("data.frame"))
     expect_that(nrow(myresults), equals(1))
     
     #test that it also works with alternate column names
     knz_001d2<-knz_001d
     names(knz_001d2)=c("sp", "yr", "sub", "abund")
-    myresults2<-VR(knz_001d2, "sub", "sp", "yr", "abund", 1)
+    myresults2<-varianceratio(knz_001d2, "sub", "sp", "yr", "abund", 1)
     expect_that(sum(myresults2$VR), equals(sum(myresults$VR)))
     
     #test that it works even if there are additional unused columns
     knz_001d3<-knz_001d
     knz_001d3$site<-"KNZ"
-    myresults3<-VR(knz_001d3, "subplot", "species", "year", "abundance", 1)
+    myresults3<-varianceratio(knz_001d3, "subplot", "species", "year", "abundance", 1)
     expect_that(myresults3$VR, is_identical_to(myresults$VR))
   
 })
