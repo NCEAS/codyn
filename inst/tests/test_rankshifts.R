d19cfbd56d0c4e71b970eb45451b2eef394342dd
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-03-03 13:00:31 -0800

    switched error to warning at line 48 in meanrankshift tests

--- inst/tests/test_rankshifts.R
+++ inst/tests/test_rankshifts.R
@@ -3,13 +3,13 @@ context("rankshifts")
 test_that("rankshifts loads and returns correct result", {
     # Ensure that trivial tests work correctly
     expect_that(length("a"), equals(1))
-    
+
     library(codyn)
-    
+
     # Load our example data set
     knz_001d <- read.csv(system.file("extdata", "knz_001d.csv", package="codyn"), sep=",", header=TRUE)
     expect_that(names(knz_001d)[4], equals("abundance"))
-    
+
     # Basic test if mean rank produces data frame with right structure and values
     result <- meanrank(knz_001d)
     expect_that(length(names(result)), equals(2))
@@ -20,7 +20,7 @@ test_that("rankshifts loads and returns correct result", {
     expect_that(nrow(result), equals(23))
     expect_that(result[[1,2]], equals(332.9921, tolerance=.00001))
     expect_that(result[[23,2]], equals(670.6388, tolerance=.00001))
-    
+
     #give new column names
   	knz_001d2 <- knz_001d
   	names(knz_001d2)=c("sp", "yr", "sub", "abund")
@@ -35,18 +35,18 @@ test_that("rankshifts loads and returns correct result", {
   	#make subplot a character
   	dat3<-dat1
   	dat3$subplot<-as.character(dat3$subplot)
-	
-	
+
+
 		#test the get_slope function
   	myresults<-meanrank(dat1, "year", "species", "abundance")
   	expect_that(class(myresults[,2]), equals("numeric"))
-  	expect_that(length(myresults), equals(2))	
+  	expect_that(length(myresults), equals(2))
   	#test that meanrank function works with different column names
   	myresults2<-meanrank(dat2,  "yr", "sp", "abund")
   	expect_that(myresults2, equals(myresults))
   	#test that gives a warning if running on factor instead of numeric
-  	expect_error(meanrank(dat2, "yr", "sp", "subplot"))
-	
+  	expect_warning(meanrank(dat2, "yr", "sp", "subplot"))
+
 		#test the meanrankshift function
   	#test that works on a single replicate
   	myresults3<-meanrankshift(dat1, replicate=NA, year="year", species="species", abundance="abundance")
@@ -85,5 +85,5 @@ test_that("rankshifts loads and returns correct result", {
   	#test that gives error when abundance column is a character or factor
   	expect_error(meanrankshift(knz_001d2, replicate="sub", year="yr", species="sp", abundance="randcharacter"))
   	expect_error(meanrankshift(knz_001d2, replicate="sub", year="yr", species="sp", abundance="randfactor"))
-    
+
 })
