9801d54170b4c1123d88d156554e642a5a0931f7
Lauren Hallett <lauren.hallett@berkeley.edu>
2015-08-03 14:19:48 -0600

    adds option to calculate synchrony following Gross et al 2014

--- R/synchrony.R
+++ R/synchrony.R
@@ -5,14 +5,17 @@
 #' @param year The name of the year column from data1
 #' @param species The name of the species column from data1
 #' @param abundance The name of the abundance column from data1
-#' @return output The degree of species synchrony, where 1 is perfect synchrony and 0 is perfect asynchrony
+#' @param metric The synchrony metric to return. The default, "Loreau", returns synchrony as calculated by Loreau and de Mazancourt 2008.
+#'        The alternative, "Gross", returns synchrony as calculated by Gross et al. 2014
+#' @return output The degree of species synchrony. If "Loreau", 1 is perfect synchrony and 0 is perfect asynchrony. 
+#'        If "Gross", 1 is perfect synchrony and -1 is perfect asynchrony.
 #' @export
-synchrony<-function(data1, replicate="replicate", species="species", year="year", abundance="abundance") {
+synchrony<-function(data1, replicate="replicate", species="species", year="year", abundance="abundance", metric="Loreau") {
   if(is.na(replicate)==TRUE){
-    output<-synch_onerep(data1, species, year, abundance)}else{
+    output<-synch_onerep(data1, species, year, abundance, metric)}else{
     data1[replicate]<-if(is.factor(data1[[replicate]])==TRUE){factor(data1[[replicate]])} else {data1[replicate]}
     X <- split(data1, data1[replicate])
-    out<-lapply(X, FUN=synch_onerep, species, year, abundance)
+    out<-lapply(X, FUN=synch_onerep, species, year, abundance, metric)
     reps<-unique(data1[replicate])
     output<-cbind(reps, do.call("rbind", out))
     names(output)=c(replicate, "synchrony")
@@ -37,8 +40,12 @@ return(output)
 #' @param year The name of the year column from data1
 #' @param species The name of the species column from data1
 #' @param abundance The name of the abundance column from data1
-#' @return output The degree of species synchrony, where 1 is perfect synchrony and 0 is perfect asynchrony
-synch_onerep<-function(data1, species, year, abundance) {
+#' @param metric The synchrony metric to return. The default, "Loreau", returns synchrony as calculated by Loreau and de Mazancourt 2008.
+#'        The alternative, "Gross", returns synchrony as calculated by Gross et al. 2014
+#' @return output The degree of species synchrony. If "Loreau", 1 is perfect synchrony and 0 is perfect asynchrony. 
+#'        If "Gross", 1 is perfect synchrony and -1 is perfect asynchrony.
+
+synch_onerep<-function(data1, species, year, abundance, metric="Loreau") {
     #remove any species that were never present
     data1<-subset(data1, abundance>0)
     #fill in 0s
@@ -48,6 +55,7 @@ synch_onerep<-function(data1, species, year, abundance) {
     data2<-merge(data1[c(species, year, abundance)], fulllist, all=T)
     data2[is.na(data2)]<-0  
   
+    if(metric=="Loreau"){
     #calculate community variance
     XTformula<-as.formula(paste(abundance, "~", year, sep=""))
     XT<-aggregate(XTformula, data=data2, sum )
@@ -60,7 +68,29 @@ synch_onerep<-function(data1, species, year, abundance) {
     varSpp<-sum((sdSpp[abundance]))*sum(sdSpp[abundance])
     
     #calculate synchrony
-    synchrony<-as.numeric(varXT/varSpp)
+    synchrony<-as.numeric(varXT/varSpp)} else{
+      if(metric=="Gross"){
+        corout<-as.data.frame(cbind(species=as.character(), "sppcor"=as.numeric()))
+        for (i in 1:nrow(spplist)){
+          myspp<-as.character(spplist[[1]][i])
+          focalspp<-data2[which(data2[species] == myspp),]
+          com.focalspp<-transpose_community(focalspp, species, year, abundance)
+          otherspp<-data2[which(data2[species] != myspp),]
+          com.otherspp<-transpose_community(otherspp, species, year, abundance)
+          agg.otherspp<-(rowSums(com.otherspp))
+          sppcor<-cor(agg.otherspp, com.focalspp)
+          ##need to add a default value for species that do not fluctuate at all
+          subout<-as.data.frame(cbind(myspp, sppcor))
+          names(subout)=c(species, "sppcor")
+          subout$sppcor<-as.numeric(as.character(subout$sppcor))    
+          corout<-rbind(corout, subout)
+        }
+        
+        #average correlation for the community
+        synchrony<-mean(corout$sppcor)
+      }
+    }
+    
     return(synchrony)
 }
 
