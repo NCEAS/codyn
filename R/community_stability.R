e0c903cdbac3f48ab548b7c1ff79f5a9751fe888
Corinna Gries <cgries@wisc.edu>
2015-10-06 17:45:42 -0500

    edit documentation, formatted indents, switched if statement to first do single rep levels then multiple

--- R/community_stability.R
+++ R/community_stability.R
@@ -1,33 +1,51 @@
-#' A function to calculate community stability over time
+#' @title Community Stability
+#' @description   A function to calculate community stability over time
+#' 
 #' @param df the data frame to use in the calculation
 #' @param time.var The name of the time column from df
 #' @param abundance.var The name of the abundance column from df
 #' @param replicate.var The name of the replicate column from df
-#' @return output The stability of community abundance, calculated as mean/standard deviation
+#' @return The stability of community abundance, calculated as mean/standard deviation returns a single number or a dataframe with the following attributes:
+#' \itemize{
+#'  \item{replicate}{replicate name}
+#'  \item{stability}{community stability for replicate}
+#'  }
+#' @details 
+#' The input dataframe needs to contain columns for time and abundance; 
 #' @export
-community_stability<-function(df, time.var="year", abundance.var="abundance", replicate.var=as.character(NA)){
-  df<-df[which(df[[abundance.var]]>0),]
-  if(is.na(replicate.var)==FALSE){
-    df<-df[order(df[[replicate.var]]),]  
-  df[replicate.var]<-if(is.factor(df[[replicate.var]])==TRUE){factor(df[[replicate.var]])} else {df[replicate.var]}
-  #sum abundance within a replicate and year
-  aggform<-as.formula(paste(abundance.var, "~", replicate.var, "+", time.var, sep=""))
-  data2<-aggregate(aggform, data=df, sum)
-  data2<-data2[order(data2[[replicate.var]]),]  
-  X<-split(data2, data2[replicate.var])
-  out<-lapply(X, stability_onerep, abundance.var)
-  reps<-unique(data2[replicate.var])
-  output<-cbind(reps, do.call("rbind", out))
-  names(output)=c(replicate.var, "stability")
-  output<-subset(output, is.na(stability)==F)} 
-  else{
-    #sum abundance within a year
-    aggform<-as.formula(paste(abundance.var, "~", time.var, sep=""))
-    data2<-aggregate(aggform, data=df, sum)
-    output<-stability_onerep(data2, abundance.var)
-  }
-  row.names(output)<-NULL
- return(output)
+
+community_stability<-function(df, time.var="year", abundance.var="abundance", replicate.var=NA){
+    stopifnot(is.numeric(df[[time.var]]))
+    stopifnot(is.numeric(df[[abundance.var]]))
+    df<-df[which(df[[abundance.var]]>0),]
+
+    if(is.na(replicate.var)==TRUE) {
+      
+        #sum abundance within a year
+        aggform<-as.formula(paste(abundance.var, "~", time.var, sep=""))
+        data2<-aggregate(aggform, data=df, sum)
+        output<-stability_onerep(data2, abundance.var)
+        
+    } else {
+    
+        df<-df[order(df[[replicate.var]]),]  
+        df[replicate.var]<-if(is.factor(df[[replicate.var]])==TRUE){factor(df[[replicate.var]])} else {df[replicate.var]}
+  
+        #sum abundance within a replicate and year
+        aggform<-as.formula(paste(abundance.var, "~", replicate.var, "+", time.var, sep=""))
+        data2<-aggregate(aggform, data=df, sum)
+        data2<-data2[order(data2[[replicate.var]]),]  
+        X<-split(data2, data2[replicate.var])
+        out<-lapply(X, stability_onerep, abundance.var)
+        reps<-unique(data2[replicate.var])
+        output<-cbind(reps, do.call("rbind", out))
+        names(output)=c(replicate.var, "stability")
+        output<-subset(output, !is.na(output$stability))
+
+    }
+    
+    row.names(output)<-NULL
+    return(output)
 }
 
 
