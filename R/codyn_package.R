952977e8d1e6ca7e5de57eb53e32c0f04ae599e4
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-10-05 17:53:29 -0700

    Remove misnamed package doc file

--- R/codyn_package.R
+++
@@ -1,8 +0,0 @@
-#' codyn title goes here
-#' @description codyn description goes here
-#' @details include details here
-#' @author Lauren Hallett
-#' @docType package
-#' @name codyn
-#' @aliases codyn
-NULL
