cd4b8b1d92195ea9c8bbb154ffd5e75f17b0769c
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-10-05 17:54:22 -0700

    more complete package level docs template

--- R/codyn-package.r
+++ R/codyn-package.r
@@ -1,5 +1,8 @@
-#' codyn.
-#'
-#' @name codyn
+#' codyn title goes here
+#' @description codyn description goes here
+#' @details include details here
+#' @author Lauren Hallett
 #' @docType package
+#' @name codyn
+#' @aliases codyn
 NULL
