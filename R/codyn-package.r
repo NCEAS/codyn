5cc6a2f77c05ac2d56874672cbfed99047eb062c
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-10-06 11:02:31 -0700

    Document example datasets

--- R/codyn-package.r
+++ R/codyn-package.r
@@ -6,3 +6,46 @@
 #' @name codyn
 #' @aliases codyn
 NULL
+
+#' Konza data from Collins et. all 2008
+#'
+#' A dataset of tallgrass prairie plant composition at one annually burned and one unburned 
+#' site over time at the Konza Prairie LTER, Manhattan Kansas (Collins et al. 2008).
+#' 
+#' \itemize{
+#'   \item replicate
+#'   \item year The sampling frequency
+#'   \item species The sampled species
+#'   \item abundance The measure of species abundance
+#' }
+#'
+#' @source
+#' Scott L. Collins 1987. Interaction of Disturbances in Tallgrass Prairie: A Field Experiment. 
+#' Ecology 68:1243–1250. http://dx.doi.org/10.2307/1939208
+#' @docType data
+#' @keywords datasets
+#' @name collins08
+#' @usage data(collins08)
+#' @format A data frame with 2058 rows and 4 variables
+NULL
+
+#' Data from Konza Prairie, watershed 001d
+#'
+#' Plant composition within multiple replicates at an annually burned tallgrass 
+#' prairie site in the Konza Prairie LTER, Manhattan KS (Watershed 001D). 
+#' 
+#' species,year,subplot,abundance
+#' \itemize{
+#'   \item species The sampled species
+#'   \item year The sampling frequency
+#'   \item subplot The sampled subplot
+#'   \item abundance The measure of species abundance
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name knz_001d
+#' @usage data(knz_001d)
+#' @format A data frame with 8768 rows and 4 variables
+NULL
+
