8f777d8133b4d627bd217d87dc9b0f0e0c5045d0
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-10-05 17:50:12 -0700

    Add package level doc (template only)

---
+++ R/codyn_package.R
@@ -0,0 +1,8 @@
+#' codyn title goes here
+#' @description codyn description goes here
+#' @details include details here
+#' @author Lauren Hallett
+#' @docType package
+#' @name codyn
+#' @aliases codyn
+NULL
