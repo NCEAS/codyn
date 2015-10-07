5a88fdcfe8821366b2ce7548d98cff969528d1d9
Matt Jones <gitcode@magisa.org>
2015-10-06 17:00:47 -0800

    Refactored get_slope to work from lagged data frame.
    The old implementation both lagged the data and calculated the slope in one function.  This is now refactored into get_intervals() that calculates the lag intervals, and get_slope() that uses those intervals.

--- R/rate_change.R
+++ R/rate_change.R
@@ -1,6 +1,6 @@
 #' A function to calculate community rate changes over time within multiple replicates
 #'
-#'#' This is an analysis of differences in species composition between samples at increasing time lags. It measures the rate of directional change in community composition. First, a triangular dissimilarity matrix is calculated using Euclidean distance. Then, the Euclidean distance values are plotted against time lags. For example, a data set with 6 time intervals will have 5 one-year time lags (year 1 vs year 2, year 2 vs year 3 ...) and 4 two-year time lags (year 1 vs year 3, year 2 vs year 4 ...). Finally, distance values are regressed against time lag. The slope of the regression line indicates the rate and direction of change.
+#' This is an analysis of differences in species composition between samples at increasing time lags. It measures the rate of directional change in community composition. First, a triangular dissimilarity matrix is calculated using Euclidean distance. Then, the Euclidean distance values are plotted against time lags. For example, a data set with 6 time intervals will have 5 one-year time lags (year 1 vs year 2, year 2 vs year 3 ...) and 4 two-year time lags (year 1 vs year 3, year 2 vs year 4 ...). Finally, distance values are regressed against time lag. The slope of the regression line indicates the rate and direction of change.
 #' @param df A dataframe containing replicate, time, species and abundance columns.
 #' @param replicate.var The name of the replicate column from df. Defaults to NA.
 #' @param time.var The name of the time column from df
@@ -30,6 +30,27 @@ rate_change <- function(df, time.var="time", species.var="species", abundance.va
     return(output)
 }
 
+# lag_intervals <- function(df, time.var="time", species.var="species", abundance.var="abundance", replicate.var=NA) {
+#     stopifnot(is.numeric(df[[time.var]]))
+#     stopifnot(is.numeric(df[[abundance.var]]))
+#     if(is.na(replicate.var)) {
+#         check_single_onerep(df, time.var, species.var)
+#         output <- get_intervals(df, time.var, species.var, abundance.var)
+#     } else {
+#         check_single(df, time.var, species.var, replicate.var)
+#         df[replicate.var] <- if(is.factor(df[[replicate.var]])) {
+#             factor(df[[replicate.var]])
+#         } else {
+#             df[replicate.var]
+#         }
+#         X <- split(df, df[replicate.var])
+#         out <- lapply(X, FUN=get_intervals, time.var, species.var, abundance.var)
+#         reps <- unique(df[replicate.var])
+#         output <- cbind(reps, do.call("rbind", out))
+#         names(output)=c(replicate.var, "rate_change")
+#     }
+#     return(output)
+# }
 
 ############################################################################
 #
@@ -39,6 +60,26 @@ rate_change <- function(df, time.var="time", species.var="species", abundance.va
 #
 ############################################################################
 
+#' Get lagged intervals
+#' Returns lagged intervals of community change within one replicate.
+#' @param df data frame to compute the slope of community change for
+#' @param time.var The name of the time column from df
+#' @param species.var The name of the species column from df
+#' @param abundance.var The name of the abundance column from df
+#' @return a data frame containing of time lags by species distances
+get_intervals <- function(df, time.var="time", species.var="species", abundance.var="abundance") {
+    df <- transpose_community(df, time.var, species.var, abundance.var)
+    DM <- dist(df[-1], method="euclidean", diag = FALSE, upper = FALSE)
+    DM <- as.matrix(DM)
+    
+    rownums = row(DM)
+    colnums = col(DM)
+    lag_list = lapply(1:(nrow(df)-1), get_lag_i, DM, rownums, colnums)
+    results <- data.frame(do.call(rbind, lag_list))
+    browser()
+    return(results)
+}
+
 #' Get slope
 #' Returns the slope of community change within one replicate.
 #' @param df data frame to compute the slope of community change for
@@ -47,15 +88,8 @@ rate_change <- function(df, time.var="time", species.var="species", abundance.va
 #' @param abundance.var The name of the abundance column from df
 #' @return a slope of time lags by species distances
 get_slope <- function(df, time.var="time", species.var="species", abundance.var="abundance") {
-    df <- transpose_community(df, time.var, species.var, abundance.var)
-    DM <- dist(df[-1], method="euclidean", diag = FALSE, upper = FALSE)
-    DM <- as.matrix(DM)
-    
-    rownums = row(DM)
-    colnums = col(DM)
-    lag_list = lapply(1:(nrow(df)-1), get_lag_i, DM, rownums, colnums)
-    
-    results <- data.frame(do.call(rbind, lag_list))
+
+    results <- get_intervals(df, time.var, species.var, abundance.var)
     lm_coefficents <- lm(value ~ lag, data=results)
     slope <- data.frame(lm_coefficents[1][[1]])
     return(slope[2,])
