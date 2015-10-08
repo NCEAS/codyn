e005b4577e67cd3036bde7dea2af182fab3f31c7
Matt Jones <gitcode@magisa.org>
2015-10-08 09:17:42 -0800

    Import the assertthat package properly.
    Also, update release date for DESCRIPTION.

--- R/utilities.R
+++ R/utilities.R
@@ -19,6 +19,7 @@ transpose_community <- function(df, time.var, species.var, abundance.var) {
 #'
 #' @param given Vector of variable names as supplied by user
 #' @param data Data frame containing variables
+#' @import assertthat
 check_names <- function(given, data) {
     for (i in given){
         assertthat::assert_that(assertthat::has_name(data, i))
