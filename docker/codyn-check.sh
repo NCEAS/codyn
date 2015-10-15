95f0cf023a7a356c3dc9e3aae903196957f67dbf
Matt Jones <gitcode@magisa.org>
2015-10-14 16:49:47 -0800

    Refactor shell script to locate package to be built and tested automatically.

--- docker/codyn-check.sh
+++ docker/codyn-check.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 # A shell wrapper to prepare and execute an R CMD check
-cd /src/dev
-R CMD build codyn
-R CMD check --as-cran codyn_0.9.0.9000.tar.gz
+srcdir=$(ls)
+R CMD build $srcdir
+pkg=$(ls *.gz)
+R CMD check --as-cran $pkg
 echo "DONE"
