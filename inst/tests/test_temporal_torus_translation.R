927daa816cca0803483876469f977119e5e8aa4b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-03-13 13:56:00 -0700

    strike out confint tests.

--- inst/tests/test_temporal_torus_translation.R
+++ inst/tests/test_temporal_torus_translation.R
@@ -100,30 +100,30 @@ test_that("temporal_torus_translation loads and returns correct result", {
   ##Test that missing levels in a factor replicate are dropped (so that the same number of subplots if run as a character
   #or as a factor with additional, missing levels)
 
-  #For example :
-  myresults2<-confint.cyclic_shift(dat1, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
-  expect_true(is.factor(myresults2$subplot))
-
-  myresults2b<-confint.cyclic_shift(dat3, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
-  expect_true(is.character(myresults2b$subplot))
-
-  expect_equal(as.character(myresults2$subplot), myresults2b$subplot)
-
-  myresults2b<-confint.cyclic_shift(knz_001d, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
-
-  #Test that is correct for whether "averagereps" is true or false
-  dat5<-dat4
-  dat5$subplot<-as.character(dat5$subplot)
-  myresults3<-confint.cyclic_shift(dat5, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=TRUE)
-  myresults4<-confint.cyclic_shift(dat5, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
-
-  #expect that subplot names are the same between input and output
-  expect_equal(unique(dat5$subplot), myresults4$subplot)
-
-  #Test both options for "averagereps"
-  myresults4<-confint.cyclic_shift(knz_001d, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=TRUE)
-  expect_that(nrow(myresults4), equals(1))
-  myresults5<-confint.cyclic_shift(knz_001d, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
-  expect_that(nrow(myresults5), equals(length(unique(knz_001d$subplot))))
+  # #For example :
+  # myresults2<-confint.cyclic_shift(dat1, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
+  # expect_true(is.factor(myresults2$subplot))
+  #
+  # myresults2b<-confint.cyclic_shift(dat3, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
+  # expect_true(is.character(myresults2b$subplot))
+  #
+  # expect_equal(as.character(myresults2$subplot), myresults2b$subplot)
+  #
+  # myresults2b<-confint.cyclic_shift(knz_001d, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
+  #
+  # #Test that is correct for whether "averagereps" is true or false
+  # dat5<-dat4
+  # dat5$subplot<-as.character(dat5$subplot)
+  # myresults3<-confint.cyclic_shift(dat5, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=TRUE)
+  # myresults4<-confint.cyclic_shift(dat5, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
+  #
+  # #expect that subplot names are the same between input and output
+  # expect_equal(unique(dat5$subplot), myresults4$subplot)
+  #
+  # #Test both options for "averagereps"
+  # myresults4<-confint.cyclic_shift(knz_001d, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=TRUE)
+  # expect_that(nrow(myresults4), equals(1))
+  # myresults5<-confint.cyclic_shift(knz_001d, replicate.var="subplot", species.var="species", time.var="year", abundance.var="abundance", FUN=variance_ratio_matrixdata, bootnumber=2, li=0.025, ui=0.975, average.replicates=FALSE)
+  # expect_that(nrow(myresults5), equals(length(unique(knz_001d$subplot))))
 
 })
