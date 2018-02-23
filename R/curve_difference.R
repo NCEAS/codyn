9eeca450ffffc5a78efd89a98327f8cc0566974e
Meghan Avolio <meghanavolio@Meghans-MB.local>
2018-02-23 12:07:13 -0500

    cleanign up the code

--- R/curve_difference.R
+++ R/curve_difference.R
@@ -95,58 +95,59 @@ if (!is.null(block.var)) {
   rep_trt <- unique(subset(df, select = c(replicate.var, treatment.var, block.var)))
 } else if (pool) {
   cross.var <- treatment.var
+  rep_trt<-unique(subset(df, select = c(replicate.var, treatment.var)))
 } else {
   cross.var <- replicate.var
+  rep_trt<-unique(subset(df, select = c(replicate.var, treatment.var)))
 }
 
 # specify aggregate formula from arguments
 
 if (pool) {
   
-  rep_trt<-unique(subset(df, select = c(replicate.var, treatment.var)))
- 
-  #need to add the casting long step...
-  #NO TIME
-  # apply fill_zeros
-  out <- fill_zeros_rep (df, replicate.var, species.var, abundance.var)
-  allsp <- merge(out, rep_trt, by=replicate.var)
-  
-  #WITH TIME
-  
-  # sort and apply fill_zeros to all time steps
+  #add zero abundnaces for missing species to get averages
   df <- df[order(df[[time.var]]),]
-  X <- split(df, df[time.var])
+  splitvars <- c(time.var)
+  X <- split(df, df[splitvars])
   out <- lapply(X, FUN = fill_zeros_rep, replicate.var, species.var, abundance.var)
   ID <- unique(names(out))
   out <- mapply(function(x, y) "[<-"(x, time.var, value = y) ,
                 out, ID, SIMPLIFY = FALSE)
   out2 <- do.call("rbind", out)
-  
   allsp <- merge(out2, rep_trt, by=replicate.var)
   
-   # specify aggregate formula from arguments
+  # specify aggregate formula from arguments
   if(is.null(time.var)) {
     by <- c(species.var, treatment.var)
   } else {
     by <- c(species.var, treatment.var, time.var)
   }
-  
   spave <- aggregate.data.frame(allsp[abundance.var], df[by], FUN = mean)
   spave <- spave[spave[[abundance.var]] != 0,]
   
-  relrankdf1<-relrank(spave, species.var, abundance.var, treatment.var, time.var)
-
-} else {
-  # rank species in each replicate
-  rep_trt <- unique(df[c(replicate.var, treatment.var, block.var)])
-  rankdf <- add_ranks_replicate(df, time.var, species.var, abundance.var, replicate.var)
-  rankdf <- merge(rankdf, rep_trt, by = replicate.var)
+  #rank each species by treatment, time
+  splitvars <- c(time.var)
+  X <- split(df, df[splitvars])
+  out <- lapply(X, FUN = relrank, species.var, abundance.var, treatment.var) 
+  ID <- unique(names(out))
+  out <- mapply(function(x, y) "[<-"(x, time.var, value = y) ,
+                out, ID, SIMPLIFY = FALSE)
+  relrankdf1 <- do.call("rbind", out) 
+} else{
+  splitvars <- c(time.var, block.var)
+  X <- split(df, df[splitvars])
+  out <- lapply(X, FUN = relrank, species.var, abundance.var, replicate.var) 
+  ID <- unique(names(out))
+  out <- mapply(function(x, y) "[<-"(x, time.var, value = y) ,
+                out, ID, SIMPLIFY = FALSE)
+  output <- do.call("rbind", out) 
+  relrankdf1 <- merge(output, rep_trt, by=replicate.var)
 }
-
+  
 # cross join for pairwise comparisons
 splitvars <- c(species.var, block.var, time.var)
 cross.var2 <- paste(cross.var, 2, sep = '')
-rankdf <- lapply(split(rankdf, rankdf[splitvars]),
+rankdf <- lapply(split(relrankdf1, relrankdf1[splitvars]),
                  function(x) {
                    y <- x
                    y[splitvars] <- NULL
@@ -155,7 +156,7 @@ rankdf <- lapply(split(rankdf, rankdf[splitvars]),
                    idx <- idx < as.integer(cross[[cross.var2]])
                    cross[idx,]
                  })
-ranktog <- do.call(rbind, c(rankdf, list(make.row.names = FALSE)))
+ranktog <- do.call(rbind, c(relrankdf1, list(make.row.names = FALSE)))
 
 # split on treatment pairs (and block if not null)
 splitvars <- c(block.var, time.var, cross.var, cross.var2)
