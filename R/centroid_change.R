e38caa5f1643ef0985f8e5c7622fc6ee3cc80d55
Meghan <meghan.avolio@gmail.com>
2018-03-14 14:25:12 -0400

    two new functions
    calculate bray-curtis dissimilarity between replicates and then average those between time steps or treamtents (between) or average them within time steps or treatments (within)

---
+++ R/centroid_change.R
@@ -0,0 +1,133 @@
+#'@title Using centoids to calucuate changes in composition and dispersion
+#' @description Calculates the changes in composition and dispersion based off a Bray-Curtis dissimilarity matrix. Composition change is the euclidean distance between the centroids of time period 1 and time period 2. Dispersion change is the difference of average dispersion of each replicate to its centroid between consecutive time periods.
+#' @param df A data frame containing time, species, abundance and replicate columns and an optional column of treatment
+#' @param time.var The name of the time column 
+#' @param species.var The name of the species column 
+#' @param abundance.var The name of the abundance column 
+#' @param replicate.var The name of the replicate column 
+#' @param treatment.var the neame of the optional treatment column
+#' @return The multivariate_change function returns a data frame with the following attributes:
+#' \itemize{
+#'  \item{time.var_pair: }{A characteric column that has the time points to be compared, separated by a dash.}
+#'  \item{composition_change: }{A numeric column that is the euclidean distance between the centroids of two consecutive time points.}
+#'  \item{dispersion_change: }{A numeric column that is the difference in the average dispersion of the replicates around the centriod for the two consecutive time periods. A negative value indicates replicates are converging over time (there is less dispersion at time peroid 2 than time period 1) and a postive value indicates replicates are diverging over time (there is more dispersion at time period 2 than time period 1.}
+#'  \item{treatment.var: }{A column that has same name and type as the treatment.var column, if treatment.var is specified.}
+#' }
+#' @examples 
+#' data(pplots)
+#' #With treatment
+#' centroid_change(pplots,
+#'                 time.var="year", 
+#'                 replicate.var = "plot", 
+#'                 treatment.var = "treatment", 
+#'                 species.var = "species", 
+#'                 abundance.var = "relative_cover")
+#' 
+#' #Without treatment
+#' df <- subset(pplots, treatment == "N1P0")
+#' centroid_change(df, 
+#'                 time.var="year", 
+#'                 replicate.var = "plot", 
+#'                 species.var = "species", 
+#'                 abundance.var = "relative_cover")
+
+#' @importFrom vegan vegdist betadisper
+#' @importFrom stats aggregate as.formula
+#' @references Avolio et al. 2015; Avolio et al. OUR PAPER, Mari Anderson?
+#' @export
+centroid_change <- function(df, time.var, species.var, abundance.var, replicate.var, treatment.var = NULL){
+  
+  # check no NAs in abundance column
+  if(any(is.na(df[[abundance.var]]))) stop("Abundance column contains missing values")
+  
+  # check unique species x time x replicate combinations
+  check_single(df, time.var, species.var, replicate.var)
+  
+  df <- as.data.frame(df)
+
+  if (is.null(treatment.var)) {
+    output <- mult_change(df, time.var, species.var, abundance.var, replicate.var)
+  } else {
+    # calculate change for each treatment
+    splitvars <- treatment.var
+    X <- split(df, df[splitvars])
+    out <- lapply(X, FUN = mult_change, time.var, species.var, abundance.var, replicate.var)
+    unsplit <- lapply(out, nrow)
+    unsplit <- rep(names(unsplit), unsplit)
+    output <- do.call(rbind, c(out, list(make.row.names = FALSE)))
+    output[splitvars] <- do.call(rbind, as.list(unsplit))
+  }
+    
+  output_order <- c(
+    paste(time.var,"pair", sep="_"),
+    treatment.var,
+    'composition_change', 'dispersion_change')
+
+  return(output[intersect(output_order, names(output))])
+}
+
+############################################################################
+#
+# Private functions: these are internal functions not intended for reuse.
+# Future package releases may change these without notice. External callers
+# should not use them.
+#
+############################################################################
+# A function calculate the community compositon change (the distance between the centriods of two consecutive time periods) and dispersion change (the difference in the average dispersion of the replicates around the centriod for the two consecutive time periods). For dispersion change a negative value indicates replicates are converging over time and a postive value indicates replicates are diverging over time.
+# @param df a dataframe
+# @param time.var the name of the time column
+# @param species.var the name of the species column
+# @param replicate.var the name of the replicate column
+mult_change <- function(df, time.var, species.var, abundance.var, replicate.var) {
+  #get years
+  timestep <- sort(unique(df[[time.var]]))
+  
+  #transpose data
+  df2<-subset(df, select = c(time.var, species.var, abundance.var, replicate.var))
+  df2$id <- paste(df2[[time.var]], df2[[replicate.var]], sep="##")
+  species <- transpose_community(df2, 'id', species.var, abundance.var)
+  species$id <- row.names(species)
+  speciesid <- do.call(rbind.data.frame, strsplit(species$id, split="##"))
+  colnames(speciesid)[1] <- "time_forfixxyz"
+  colnames(speciesid)[2] <- replicate.var
+  speciesid[[time.var]]<-as.numeric(as.character(speciesid$time_forfixxyz))
+  speciesid.2 <- subset(speciesid, select = -time_forfixxyz)
+  species2 <- cbind(speciesid.2, species)
+  species3 <- subset(species2, select = -id)
+  
+  #calculate bray-curtis dissimilarities
+  bc <- vegdist(species3[,3:ncol(species3)], method="bray")
+  
+  #calculate distances of each plot to year centroid (i.e., dispersion)
+  disp <- betadisper(bc, species3[[time.var]], type="centroid")
+  
+  #getting distances between centroids over years; these centroids are in BC space, so that's why this uses euclidean distances
+  cent_dist <- as.matrix(vegdist(disp$centroids, method="euclidean"))
+  
+  ##extracting only the comparisions, year x to year x+1.
+  cent_dist_yrs <- data.frame(
+    time1 = timestep[1:length(timestep)-1],
+    time2 = timestep[2:length(timestep)],
+    composition_change = diag(
+      as.matrix(cent_dist[2:nrow(cent_dist), 1:(ncol(cent_dist)-1)])))
+  
+  #collecting and labeling distances to centroid from betadisper to get a measure of dispersion and then take the mean for a year
+  disp2 <- data.frame(time=species2[[time.var]],
+                   dist = disp$distances)
+  
+  myformula <- as.formula(paste("dist", "~", "time"))
+  disp2.2<-aggregate(myformula, mean, data=disp2)
+  
+  ##subtract consecutive years (x+1 - x). A positive value indicates greater dispersion in year x+1 and a negative value indicates less dispersion in year x+1
+  disp_yrs <- data.frame(time2 = timestep[2:length(timestep)],
+                      dispersion_change = diff(disp2.2$dist))
+  
+  #merge together change in mean and dispersion data
+  distances <- merge(cent_dist_yrs, disp_yrs, by="time2")
+  distances$time_pair<-paste(distances$time1, distances$time2, sep="-")
+  
+  distances<-subset(distances, select = c("time_pair", "composition_change", "dispersion_change"))
+  colnames(distances)[1]<-paste(time.var, "pair", sep="_")
+  
+  return(distances)
+}
---
+++ R/dissimilarity_difference.R
@@ -0,0 +1,130 @@
+#'@title Bray curtis dissimilarity differences of replicates between and within treatments
+#'@description Calculates the average difference in Bray-Curtis dissimilarity of replicates treatments and within treatments at a single point in time. Between treatment differences is the average Bray-Curtis dissimilaritiy of all pairwise comparisions of replicates between two treatments. This is a measure of how dissimilar the community composition of two treatments is. Bray-Curtis dissimilarity ranges from 0-1, where 0 are identical communities, and 1 and completelty different communiites. Within difference is derived in two steps. First, the average Bray-Curtis dissimilarity of all replicates within a treatment is calculated. This is a measure of how homogenous a community is within a treatment. Then, these averages are compared between two treatments.
+#' @param df A data frame containing an optional time column, species, abundance and replicate, and treatment columns
+#' @param time.var The name of the optional time column 
+#' @param species.var The name of the species column 
+#' @param abundance.var The name of the abundance column 
+#' @param replicate.var The name of the replicate column 
+#' @param treatment.var the name of the treatment column
+#' @return The multivariate_difference function returns a data frame with the following attributes:
+#' \itemize{
+#'  \item{treatment.var: }{A column that has same name and type as the treatment.var column, if treatment.var is specified.}
+#'  \item{treatment.var2: }{A column that has the same type as the treatment.var column, and is named treatment.var with a 2 appended to it.}
+#'  \item{BC_between_diff: }{A numeric column that is the average pairwise Bray-Curtis dissimilarity of replicates in two treatments. 0 - The communities of treatments are similar. 1 - The communities of treatments are very different.}
+#'  \item{BC_within_diff: }{A numeric column that is difference between two treatments in the average pairwise Bray-Curtis dissimilarity of replicates within each treatment. A negative number indicates that treatment.var2 has varaiblity in community composition than treatment.var.}
+#'  \item{time.var: }{A characteric column that has the same name and type as the time.var column, if specified.}
+#' }
+#' @references Our Avolio et al. paper, Avolio et al. 2015, Marti Anderson?
+#' @importFrom vegan vegdist
+#' @importFrom stats aggregate as.formula
+#' @examples  
+#' data(pplots)
+#' #Without time
+#' df <- subset(pplots, year == 2002)
+#' dissimilarity_difference(df, 
+#'                         replicate.var = "plot", 
+#'                         treatment.var = "treatment", 
+#'                         species.var = "species", 
+#'                         abundance.var = "relative_cover")
+#' #With time
+#' dissimilarity_difference(pplots, 
+#'                         time.var = "year", 
+#'                         replicate.var = "plot", 
+#'                         species.var = "species", 
+#'                         abundance.var = "relative_cover", 
+#'                         treatment.var = "treatment")
+#' @export
+dissimilarity_difference <- function(df, time.var = NULL, species.var,
+                                    abundance.var, replicate.var, treatment.var) {
+  
+  # check no NAs in abundance column
+  if(any(is.na(df[[abundance.var]]))) stop("Abundance column contains missing values")
+  
+  if(is.null(time.var)){
+    
+    # check there unique species x time combinations
+    check_single_onerep(df, replicate.var, species.var)
+    
+    output <- dissim_diff(df, species.var, abundance.var, replicate.var, treatment.var)
+    
+  } else {
+    
+    # check unique species x time x replicate combinations
+    check_single(df, time.var, species.var, replicate.var)
+    
+    splitvars <- time.var
+    X <- split(df, 
+               df[splitvars])
+    out <- lapply(X, FUN = dissim_diff, species.var, abundance.var, replicate.var, treatment.var)
+    unsplit <- lapply(out, nrow)
+    unsplit <- rep(names(unsplit), unsplit)
+    output <- do.call(rbind, c(out, list(make.row.names = FALSE)))
+    output[splitvars] <- do.call(rbind, as.list(unsplit))
+  }
+  
+  output_order <- c(
+    time.var,
+    treatment.var, paste(treatment.var, 2, sep = ''),
+    'BC_between_diff', 'BC_within_diff')
+  
+  return(output[intersect(output_order, names(output))])
+}
+
+############################################################################
+#
+# Private functions: these are internal functions not intended for reuse.
+# Future package releases may change these without notice. External callers
+# should not use them.
+#
+############################################################################
+
+# A function calculate the average Bray-Curtis dissimilarity of all pairwise comparisions of replicates either between two treatments or within a treatment. Once this is calcualted, for within_diff, the differences of within_dissimliarity is calculated betweent two treatments.
+# @param df a dataframe
+# @param species.var the name of the species column
+# @param replicate.var the name of the replicate column
+# @param treatment.var the name of the treatment column
+
+dissim_diff <- function(df, species.var, abundance.var, replicate.var, treatment.var){
+
+  #transpose data
+  df2<-subset(df, select = c(species.var, abundance.var, replicate.var, treatment.var))
+  df2$id <- paste(df2[[treatment.var]], df2[[replicate.var]], sep="##")
+  species <- codyn:::transpose_community(df2, 'id', species.var, abundance.var)
+ 
+  #calculate BC dissimilarity
+  bc <- as.data.frame(as.matrix(vegdist(species, method="bray")))
+  
+  #extracting lower diagonal
+  bc2 <- as.data.frame(cbind(rownames(bc)[which(lower.tri(bc), arr.ind=T)[,1]],
+                             colnames(bc)[which(lower.tri(bc), arr.ind=T)[,2]],
+                             bc[lower.tri(bc)]))
+  c1 <- as.data.frame(do.call('rbind', strsplit(as.character(bc2$V1), "##", fixed = T)))
+  c2 <- as.data.frame(do.call('rbind', strsplit(as.character(bc2$V2), "##", fixed = T)))
+  
+  bc3 <- cbind(bc2, c1, c2)
+  bc3$bc_dissim <- as.numeric(as.character(bc3$V3))
+  colnames(bc3)[4] <- paste(treatment.var, 2, sep="")
+  colnames(bc3)[6] <- treatment.var
+  bc3$compare <- ifelse(bc3[[treatment.var]] == bc3[[paste(treatment.var, 2, sep="")]], 1, 2)
+  
+  #within treatment differences
+  bc_within <- subset(bc3, compare == 1)
+  myformula <- as.formula(paste("bc_dissim", "~", treatment.var))
+  bc_within_ave <- aggregate(myformula, mean, data=bc_within)
+  colnames(bc_within_ave)[2] <- "BC_dissim_within"
+  
+  #between treatment differences
+  bc_between <- subset(bc3, compare == 2)
+  myformula2 <- as.formula(paste("bc_dissim", "~", treatment.var, "+", paste(treatment.var, 2, sep = "")))
+  bc_between_ave <- aggregate(myformula2, mean, data=bc_between)
+  colnames(bc_between_ave)[3] <- "BC_between_diff"
+  
+  #mege into get bc_within differences for each treatment
+  bc_dis1 <- merge(bc_between_ave, bc_within_ave, by = treatment.var)
+  bc_dis <- merge(bc_dis1, bc_within_ave, by.x = paste(treatment.var, 2, sep = ""), by.y = treatment.var)
+  bc_dis$BC_within_diff <- bc_dis$BC_dissim_within.x - bc_dis$BC_dissim_within.y
+  bc_dis$BC_dissim_within.x <- NULL
+  bc_dis$BC_dissim_within.y <- NULL
+
+  return(bc_dis)
+}
