f144cb522a930f274f9391f8008f60258096f2b2
itcarroll <icarroll@sesync.org>
2019-01-08 23:09:59 -0500

    remove help for non exported functions

--- R/utilities.R
+++ R/utilities.R
@@ -1,10 +1,10 @@
-#' Convert from a long form abundance dataframe to a time by species dataframe.
-#'
-#' @param df A dataframe containing time.var, species.var and abundance.var columns
-#' @param time.var The name of the time column from df
-#' @param species.var The name of the species column from df
-#' @param abundance.var The name of the abundance column from df
-#' @return A dataframe of species abundances x time
+# Convert from a long form abundance dataframe to a time by species dataframe.
+#
+# @param df A dataframe containing time.var, species.var and abundance.var columns
+# @param time.var The name of the time column from df
+# @param species.var The name of the species column from df
+# @param abundance.var The name of the abundance column from df
+# @return A dataframe of species abundances x time
 transpose_community <- function(df, time.var,
                                 species.var,
                                 abundance.var) {
@@ -25,22 +25,22 @@ transpose_community <- function(df, time.var,
   return(comdat)
 }
 
-#' check names of data frames
-#'
-#' @param given Vector of variable names as supplied by user
-#' @param data Data frame containing variables
+# check names of data frames
+#
+# @param given Vector of variable names as supplied by user
+# @param data Data frame containing variables
 check_names <- function(given, data) {
     for (i in given) {
         assertthat::assert_that(assertthat::has_name(data, i))
     }
 }
 
-#' Utility function to warn users that either multiple records exist within
-#' replicates, or that data may be spanning multiple replicates but no
-#' replicate.var has been specified
-#' @param df A dataframe containing time.var, species.var and abundance.var columns
-#' @param time.var The name of the time column from df
-#' @param species.var The name of the species column from df
+# Utility function to warn users that either multiple records exist within
+# replicates, or that data may be spanning multiple replicates but no
+# replicate.var has been specified
+# @param df A dataframe containing time.var, species.var and abundance.var columns
+# @param time.var The name of the time column from df
+# @param species.var The name of the species column from df
 check_single_onerep <- function(df, time.var, species.var) {
   counts <- table(df[[time.var]], df[[species.var]])
   if (any(counts > 1))
@@ -49,15 +49,15 @@ check_single_onerep <- function(df, time.var, species.var) {
                sep = ' '))
 }
 
-#' Utility function to ensure only a single record exists for a given species
-#' within one replicate if replicate.var given, and for one time point if
-#' time.var given.
-#' @param df A dataframe containing a species.var column, and optionally a
-#'   time.var and replicate.var columns
-#' @param species.var The name of the species column from df
-#' @param time.var The name of the time column from df
-#' @param replicate.var The name of the replicate column from df
-#'
+# Utility function to ensure only a single record exists for a given species
+# within one replicate if replicate.var given, and for one time point if
+# time.var given.
+# @param df A dataframe containing a species.var column, and optionally a
+#   time.var and replicate.var columns
+# @param species.var The name of the species column from df
+# @param time.var The name of the time column from df
+# @param replicate.var The name of the replicate column from df
+#
 #' @importFrom utils capture.output
 check_single <- function(df,
                          species.var,
@@ -94,21 +94,20 @@ check_single <- function(df,
   }
 }
 
-#' Utility to check for numeric abundance and time variables
-#'
-#' @param df A dataframe containing time.var, species.var, and replicate.var columns
-#' @param time.var The name of the time column from df
-#' @param abundance.var The name of the replicate column from df
-
+# Utility to check for numeric abundance and time variables
+#
+# @param df A dataframe containing time.var, species.var, and replicate.var columns
+# @param time.var The name of the time column from df
+# @param abundance.var The name of the replicate column from df
 check_numeric <- function(df, time.var, abundance.var) {
   if(!is.numeric(df[[time.var]])) { stop("Time variable is not numeric") }
   if(!is.numeric(df[[abundance.var]])) { stop("Abundance variable is not numeric") }
   }
 
-#' Utility function to stop calculations if only one species occurs in at least one replicate
-#' @param df A dataframe containing time.var, species.var and abundance.var columns
-#' @param species.var The name of the species column from df
-#' @param replicate.var The name of the replicate column from df
+# Utility function to stop calculations if only one species occurs in at least one replicate
+# @param df A dataframe containing time.var, species.var and abundance.var columns
+# @param species.var The name of the species column from df
+# @param replicate.var The name of the replicate column from df
 check_multispp <- function(df, species.var, replicate.var){
 
   df <- droplevels(df)
@@ -120,8 +119,8 @@ check_multispp <- function(df, species.var, replicate.var){
   }
 }
 
-#' Utility function to stop calculations if the species never change in a replicate
-#' @param comdat A community dataframe
+# Utility function to stop calculations if the species never change in a replicate
+# @param comdat A community dataframe
 #' @importFrom stats var
 check_sppvar <- function(comdat){
   sppvar <- sum(apply(comdat, 2, var))
@@ -129,24 +128,24 @@ check_sppvar <- function(comdat){
     stop("One or more replicates consist of species that never vary;
          please remove these replicates before calculation")}
 
-#' Utility function to calculate richness
-#' @param x Vector of abundance of each species
+# Utility function to calculate richness
+# @param x Vector of abundance of each species
 S <- function(x){
   x <- x[x > 0 & !is.na(x)]
   stopifnot(x == as.numeric(x))
   length(x)
 }
 
-#' Add zero abundances for missing species, on the assumption that any species
-#' in the \code{species.var} column should be included for every group defined
-#' by all the remaining columns save \code{abundance.var}.
-#'
-#' @param df A dataframe with species, abundances, and at least one other column
-#'   to group by
-#' @param species.var The name of the species column from df
-#' @param abundance.var The name of the abundance column from df
-#' @return A dataframe with the same columns as df, but with zeros added for
-#'   species that are present in df, but not in a particular group.
+# Add zero abundances for missing species, on the assumption that any species
+# in the \code{species.var} column should be included for every group defined
+# by all the remaining columns save \code{abundance.var}.
+#
+# @param df A dataframe with species, abundances, and at least one other column
+#   to group by
+# @param species.var The name of the species column from df
+# @param abundance.var The name of the abundance column from df
+# @return A dataframe with the same columns as df, but with zeros added for
+#   species that are present in df, but not in a particular group.
 fill_zeros <- function(df, species.var, abundance.var) {
 
   mergevars <- !(names(df) %in% c(species.var, abundance.var))
@@ -157,16 +156,16 @@ fill_zeros <- function(df, species.var, abundance.var) {
   return(df)
 }
 
-#' Add missing abundances for species absent from a replicate, on the assumption
-#' that any species in the \code{species.var} column should be included for
-#' every group defined by all the remaining columns except \code{abundance.var}.
-#'
-#' @param df A dataframe with species, abundances, and at least one other column
-#'   to group by
-#' @param species.var The name of the species column from df
-#' @param abundance.var The name of the abundance column from df
-#' @return A dataframe with the same columns as df, but with \code{NA} added for
-#'   species that are present in df, but not in each group.
+# Add missing abundances for species absent from a replicate, on the assumption
+# that any species in the \code{species.var} column should be included for
+# every group defined by all the remaining columns except \code{abundance.var}.
+#
+# @param df A dataframe with species, abundances, and at least one other column
+#   to group by
+# @param species.var The name of the species column from df
+# @param abundance.var The name of the abundance column from df
+# @return A dataframe with the same columns as df, but with \code{NA} added for
+#   species that are present in df, but not in each group.
 fill_species <- function(df, species.var, abundance.var) {
 
   mergevars <- !(names(df) %in% c(species.var, abundance.var))
@@ -176,22 +175,22 @@ fill_species <- function(df, species.var, abundance.var) {
   return(df)
 }
 
-#' @title Add abundance ranks
-#' @description Rank species by abundance, by specified grouping. Species with
-#'   zero abundance receive rank S+1, where S is the total number of species in
-#'   the group.
-#' @param df A data frame containing a single record per species with its abundance
-#' @param abundance.var The name of the abundance column
-#'
-#' @return The add_ranks function returns a data frame with the following
-#'   additional column:
-#'   \itemize{
-#'     \item{rank: }{A numeric column with the species rank; a rank of 1
-#'     indicates the species was most abundant in that time period. All species
-#'     that are not present in that time period have the rank value S+1 where S
-#'     is the number of species in the sample.
-#'     }
-#'   }
+# @title Add abundance ranks
+# @description Rank species by abundance, by specified grouping. Species with
+#   zero abundance receive rank S+1, where S is the total number of species in
+#   the group.
+# @param df A data frame containing a single record per species with its abundance
+# @param abundance.var The name of the abundance column
+#
+# @return The add_ranks function returns a data frame with the following
+#   additional column:
+#   \itemize{
+#     \item{rank: }{A numeric column with the species rank; a rank of 1
+#     indicates the species was most abundant in that time period. All species
+#     that are not present in that time period have the rank value S+1 where S
+#     is the number of species in the sample.
+#     }
+#   }
 add_ranks <- function(df, abundance.var) {
 
   # get species richness, note that zero abunance does not contribute to S
@@ -204,23 +203,23 @@ add_ranks <- function(df, abundance.var) {
   return(df)
 }
 
-#' @title Add relative abundance ranks and cumulative abundance
-#' @description Rank species by abundance within the specified grouping. The
-#'   rank of the lead abundant species is 1 and the most abundant species has
-#'   rank 1/S, where S is the number of species in the group.
-#' @param df A data frame containing a single record per species with its abundance
-#' @param abundance.var The name of the abundance column
-#' @param species.var The name of the species column
-#'
-#' @return The add_rank_abundance function returns a data frame with the following
-#'   additional column:
-#'   \itemize{
-#'     \item{rank: }{}
-#'     \item{relrank: }{A numeric column with the species rank divided
-#'   by the maximum rank in the group; a rank of 1 indicates the species was the
-#'   least abundant.}
-#'     \item{cumabund: }{}
-#'   }
+# @title Add relative abundance ranks and cumulative abundance
+# @description Rank species by abundance within the specified grouping. The
+#   rank of the lead abundant species is 1 and the most abundant species has
+#   rank 1/S, where S is the number of species in the group.
+# @param df A data frame containing a single record per species with its abundance
+# @param abundance.var The name of the abundance column
+# @param species.var The name of the species column
+#
+# @return The add_rank_abundance function returns a data frame with the following
+#   additional column:
+#   \itemize{
+#     \item{rank: }{}
+#     \item{relrank: }{A numeric column with the species rank divided
+#   by the maximum rank in the group; a rank of 1 indicates the species was the
+#   least abundant.}
+#     \item{cumabund: }{}
+#   }
 #' @importFrom stats stepfun
 add_rank_abundance <- function(df, species.var, abundance.var) {
 
@@ -236,17 +235,17 @@ add_rank_abundance <- function(df, species.var, abundance.var) {
   return(out)
 }
 
-#' @title A Split-Apply-Combine implementation
-#' @description Faster split-apply-combine for data frames, when the results of FUN
-#' are homogeneous with respect to the number, order, data type and (if
-#' applicable) levels of columns in the returned data frame.
-#'
-#' @param df A data frame
-#' @param by The name of column(s) in the data frame that define groups to split
-#' @param FUN The function applied to each grouped data frame after splitting
-#' @param ... Additional parameters to FUN
-#'
-#' @source \url{https://stackoverflow.com/a/9730292/687112}
+# @title A Split-Apply-Combine implementation
+# @description Faster split-apply-combine for data frames, when the results of FUN
+# are homogeneous with respect to the number, order, data type and (if
+# applicable) levels of columns in the returned data frame.
+#
+# @param df A data frame
+# @param by The name of column(s) in the data frame that define groups to split
+# @param FUN The function applied to each grouped data frame after splitting
+# @param ... Additional parameters to FUN
+#
+# @source \url{https://stackoverflow.com/a/9730292/687112}
 split_apply_combine <- function(df, by, FUN, ...) {
   if (is.null(by)) {
     # just apply
@@ -269,10 +268,10 @@ split_apply_combine <- function(df, by, FUN, ...) {
   as.data.frame(out)
 }
 
-#' @title Rank-Abundance Curve Dissimilarity
-#' @description A function to calculate the curve difference between two communities, given the empirical relative rank by abundance curve as a \code{stepfun}.
-#' @param sf The curve for the first community.
-#' @param sf2 The curve for the second community.
+# @title Rank-Abundance Curve Dissimilarity
+# @description A function to calculate the curve difference between two communities, given the empirical relative rank by abundance curve as a \code{stepfun}.
+# @param sf The curve for the first community.
+# @param sf2 The curve for the second community.
 curve_dissim <- function(sf, sf2) {
 
   relrank <- get('x', envir = environment(sf))
@@ -284,10 +283,10 @@ curve_dissim <- function(sf, sf2) {
   return(sum(w*h))
 }
 
-#' @title Evar
-#' @description A utility function to calculate Evar from Smith and Wilson 1996
-#' @param S the number of species in the sample
-#' @param x the vector of abundances of each species
+# @title Evar
+# @description A utility function to calculate Evar from Smith and Wilson 1996
+# @param S the number of species in the sample
+# @param x the vector of abundances of each species
 Evar <- function(x, S = length(x)) {
   x1 <- x[x!=0]
   lnx <- log(x1)
@@ -295,20 +294,19 @@ Evar <- function(x, S = length(x)) {
   return(1 - 2 / pi * atan(theta))
 }
 
-#' @title Checking for errors in arguments
-#' @description Check for errors in the application of arguments and input data
-#'   for all \code{*_change} and \code{*_difference} functions.
-#' @param df The name of the dataframe
-#' @param time.var The name of the optional time column
-#' @param species.var The name of the species column
-#' @param abundance.var The name of the abundance column
-#' @param replicate.var The name of the replicate column
-#' @param treatment.var The name of the optional treatment column
-#' @param block.var The name of the optional block column
-#' @param pool The name the optional pooling approach
-#' @param reference.treatment The name of the optional reference treatment
-#' @param reference.time The name of the optional reference time
-
+# @title Checking for errors in arguments
+# @description Check for errors in the application of arguments and input data
+#   for all \code{*_change} and \code{*_difference} functions.
+# @param df The name of the dataframe
+# @param time.var The name of the optional time column
+# @param species.var The name of the species column
+# @param abundance.var The name of the abundance column
+# @param replicate.var The name of the replicate column
+# @param treatment.var The name of the optional treatment column
+# @param block.var The name of the optional block column
+# @param pool The name the optional pooling approach
+# @param reference.treatment The name of the optional reference treatment
+# @param reference.time The name of the optional reference time
 check_args <- function(df,
                        time.var = NULL,
                        species.var,
