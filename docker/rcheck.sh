509ab00152b07feb339c6c103c87672b86a56727
Ian Carroll <icarroll@sesync.org>
2019-03-03 17:36:16 -0500

    refactor to use rocker/verse and docker-compose up
    The cboettig/texlive image was not found, but the rocker/verse image includes tex and (I presume) made the link to a tex container unecessary.
    
    `docker-compose up` (unlike run) allows you to keep using the same container, with package deps installed.

--- docker/rcheck.sh
+++ docker/rcheck.sh
@@ -1,8 +1,13 @@
-#/bin/sh
-# A shell wrapper to prepare and execute an R CMD check
-srcdir=$(ls)
-xvfb-run R CMD build $srcdir
-pkg=$(ls *.gz)
+#!/bin/bash
+
+# install R package dependencies 
+Rscript --vanilla -e " \
+  options(repos = c(CRAN = 'https://cran.rstudio.com')); \
+  devtools::install_dev_deps(\"$1\")"
+
+#  prepare and execute an R CMD check
+xvfb-run R CMD build $1
+pkg=$(ls $1*.gz)
 xvfb-run R CMD check --as-cran $pkg
 rm $pkg
 echo "DONE"
